6ad693ec82f4e23affd101bbbd0264d434b174de
geekvaibhav <vaibhav.singh144@gmail.com>
2017-05-23 10:53:44 +0530

    initialcommit

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAcEl6o4PAwBAOnIAAO8gKKrjpFctvYqEDBH7hoLpM5uOjoS42fDeNwDMirHrZAeotfCKbpYIZAgfcO6uuHAbnDMI659yGWX1zCxlr3dmxSAp5eAZAGGkG5RVZBBWPwrOubpS1iMk4sh39SdCmLOgD3bCDSZA1ltnvHbkb5l4rwZDZD',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: 'a26c3ef325dd4758bbbf9930c2b58a8e',
+    FB_APP_SECRET: '24b371ee1cd2768e770994a865053bd2',
+    SERVER_URL: "http://salty-hollows-44528.herokuapp.com/",
+    SENGRID_API_KEY: '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',
+    EMAIL_FROM: 'jana@ucan-courses.com',
+    EMAIL_TO: 'vaibhav.singh144@gmail.com',
+    WEATHER_API_KEY: 'b687283f063884d31e0f527129779ef2',
+};
